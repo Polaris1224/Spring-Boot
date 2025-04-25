c9b7c7507ebb3949eda2f5f40083f27cfe263b96
Polaris <118846149+Polaris1224@users.noreply.github.com>
2025-04-25 17:40:13 +0530

    Delete OrderMicroservice/OrderMicroservice directory

--- OrderMicroservice/OrderMicroservice/src/main/java/com/example/OrderMicroservice/Repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.OrderMicroservice.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.OrderMicroservice.Entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long>{
-
-}
